c094ef9990eb5d96b4dca98314db8b26f8e4b3df
BioInfo-WStation-02 <andrew.watson@nibrt.ie>
2018-09-25 15:17:06 +0100

    initial commit

---
+++ build.py
@@ -0,0 +1,30 @@
+from pybuilder.core import use_plugin, init, Author
+
+use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.install_dependencies")
+use_plugin("python.flake8")
+use_plugin("python.coverage")
+use_plugin("python.distutils")
+
+name    = "MyProject"
+version = "1.0"
+
+summary = "Example PyBuilder / Git project"
+url     = "https://github.com/awwsmm/PybGit"
+
+description = """An example PyBuilder / Git project for project management
+and file version control. See blog post at <INSERT> for a more through
+explanation."""
+
+authors      = [Author("Andrew Watson", "andrew.watson@nibrt.ie")]
+license      = "None"
+default_task = "publish"
+
+@init
+def initialize(project):
+    project.build_depends_on("mockito")
+
+@init
+def set_properties(project):
+    pass
